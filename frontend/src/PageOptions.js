bbd90b7abd595a84a4dbf692a7531e0a53f6185f
jasperchan5 <jscnn51011@gmail.com>
2021-12-22 02:13:19 +0800

    Calendar improvement

--- frontend/src/PageOptions.js
+++ frontend/src/PageOptions.js
@@ -3,7 +3,8 @@ import { Button, Input, Calendar, Layout, Modal } from "antd"
 import { Header, Content, Footer } from 'antd/lib/layout/layout';
 import "../node_modules/antd/dist/antd.css";
 import SearchType from './SearchTypePage';
-import CalendarModal from './CalendarModal';
+import CalendarModal from './CalendarFuncs/CalendarModal';
+import moment from 'moment';
 
 const Options = ({setLogin, teamName}) => {
     //是否是選擇時間登記
@@ -12,7 +13,9 @@ const Options = ({setLogin, teamName}) => {
     const [search, setSearch] = useState(false);
     const [currentDate, setCurrentDate] = useState("");
     const [isModalVisible,setIsModalVisible] = useState(false);
-    
+
+    const selectedDays = [];
+
     const OptionPage = <>
         <Layout>
             <Header className="system__title">功能列表</Header>
@@ -39,11 +42,38 @@ const Options = ({setLogin, teamName}) => {
             <Header className="system__title">{currentDate}競賽匹配系統</Header>
             <br></br>
             <Layout>
-                <Content className='system__calendar'><Calendar fullscreen={false} onSelect={(e)=>{
-                    // setCurrentDate(e);
-                    setIsModalVisible(true);
-                    console.log(isModalVisible);
-                    }}></Calendar>{isModalVisible?<CalendarModal></CalendarModal>:<></>}</Content>
+                <Content className='system__calendar'><Calendar 
+                fullscreen={false} 
+                onSelect={(e)=>{
+                    let curr = [e.year(),e.month(),e.date()];
+                    let found = false, index = -1;
+                    for(let i=0;i<selectedDays.length;i++){
+                        if(selectedDays[i][0] === curr[0] && selectedDays[i][1] === curr[1] && selectedDays[i][2] === curr[2]){
+                            found = true;
+                            index = i;
+                        }
+                    }
+                    if(found){
+                        selectedDays.splice(index,1);
+                    }
+                    else{
+                        selectedDays.push(curr);
+                    }
+                    console.log(selectedDays);
+                    }}
+                disabledDate={(time) => {
+                    return time < moment().subtract(1,"days");
+                }}
+                dateCellRender={(e) => {
+                    for(let i=0;i<selectedDays.length;i++){
+                        if(e.year() === selectedDays[i][0] && e.month() === selectedDays[i][1] && e.date() === selectedDays[i][2]){
+                            return(
+                                <h5>√</h5>
+                            )
+                        }
+                    }
+                }}
+                    ></Calendar></Content>
             </Layout>
             <Footer className='col-md-12 system__title'>   
                 <Button className="system__margins" onClick={() => {
@@ -59,20 +89,46 @@ const Options = ({setLogin, teamName}) => {
         <Header className="system__title">{currentDate}競賽匹配系統</Header>
             <br></br>
             <Layout>
-                <Content className='system__calendar'><Calendar fullscreen={false} onSelect={(e)=>{
-                    // setCurrentDate(e);
-                    setIsModalVisible(true);
-                    }}></Calendar></Content>
+            <Content className='system__calendar'><Calendar 
+                fullscreen={false} 
+                onSelect={(e)=>{
+                    let curr = [e.year(),e.month(),e.date()];
+                    let found = false, index = -1;
+                    for(let i=0;i<selectedDays.length;i++){
+                        if(selectedDays[i][0] === curr[0] && selectedDays[i][1] === curr[1] && selectedDays[i][2] === curr[2]){
+                            found = true;
+                            index = i;
+                        }
+                    }
+                    if(found){
+                        selectedDays.splice(index,1);
+                    }
+                    else{
+                        selectedDays.push(curr);
+                    }
+                    console.log(selectedDays);
+                    }}
+                disabledDate={(time) => {
+                    return time < moment().subtract(1,"days");
+                }}
+                dateCellRender={(e) => {
+                    for(let i=0;i<selectedDays.length;i++){
+                        if(e.year() === selectedDays[i][0] && e.month() === selectedDays[i][1] && e.date() === selectedDays[i][2]){
+                            return(
+                                <h5>√</h5>
+                            )
+                        }
+                    }
+                }}
+                    ></Calendar></Content>
             </Layout>
             <Footer className='col-md-12 system__title'>
                 <Button className="system__margins" onClick={() => {
-                    setRegister(false);
                     }}>Add new team</Button>    
                 <Button className="system__margins" onClick={() => {
                     setRegister(false);
                     }}>Back to options</Button>
                 <Button className="system__margins" onClick={() => {
-                    setRegister(false);
                     }}>Delete team</Button>
             </Footer>
             <Footer><h5 style={{fontFamily: "sans-serif"}}>Now log in as: {teamName}</h5></Footer>
