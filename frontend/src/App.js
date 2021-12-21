bb3e788356c9b867faf2fd3c7e4c53509c596a89
jasperchan5 <jscnn51011@gmail.com>
2021-12-21 12:53:57 +0800

    Add calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import './bootstrap.css'
-import jquery from 'jquery'
-import $ from 'jquery'
-import moment from 'moment'
-import fullcalendar from 'fullcalendar'
+import Calendar from './Calendar/Calendar.js';
 
 function App() {
-
-  useEffect(() => {
-    $('#calendar').fullCalendar();
-  }, []);
   
   return (
     <div>
       <div className="system__title">競賽匹配系統</div>
-      <div id="calendar"></div>
+      <div className="row"><Calendar/></div>
     </div>
     
   );
