e7f0bc52bd781a21ccf190b7845d28a8b5da94ed
Forbes Chou <seer0970202206@gmail.com>
2021-12-22 00:40:58 +0800

    Add search

--- frontend/src/PageOptions.js
+++ frontend/src/PageOptions.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react';
 //import Calendar from './Calendar/Calendar.js';
 import {Button, Input, Calendar} from "antd"
 import "../node_modules/antd/dist/antd.css";
+import SearchType from './SearchTypePage';
 
 const Options = ({setLogin, teamName}) => {
+    //是否是選擇時間登記
     const [register, setRegister] = useState(false);
+    //是否是選擇結果查詢
     const [search, setSearch] = useState(false);
-    const [queryValue,setQueryValue] = useState("");
+  
+   
 
     const OptionPage = <>
         <div className="system__title">功能列表</div>
@@ -52,26 +56,13 @@ const Options = ({setLogin, teamName}) => {
         </div>
     </>
 
-    const SearchPage = <>
-        <div className="system__title">查詢匹配結果</div>
-        <div className="system__app">
-            <Input.Search
-                placeholder='Enter your team name'
-                onChange={(e) => {setQueryValue(e.target.value)}}
-                onSearch={() => alert(queryValue)}
-            ></Input.Search>
-            <Button className="system__margins" onClick={() => {
-                setSearch(false);
-                }}>Back to options</Button>
-        </div>
-    </>
     return (<>
         <h5 style={{fontFamily: "sans-serif"}}>Now log in as: {teamName}</h5>
-        {register?(teamName === "Admin"?AdminMainPage:MainPage):(search?SearchPage:OptionPage)}
+        {register?(teamName === "Admin"?AdminMainPage:MainPage):(search?<SearchType setSearch={setSearch}/>:OptionPage)}
     </>)
 }
 
-export default Options
+export default Options;
 
 
 
