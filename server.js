f08c604712b7fa3386da88693201e3ffb660043f
jasperchan5 <jscnn51011@gmail.com>
2022-01-13 22:57:51 +0800

    Add code

--- server.js
+++ server.js
@@ -1,38 +1,14 @@
-import express from "express";
-import { ApolloServer, PubSub } from "apollo-server-express";
-import { importSchema } from "graphql-import";
-import bodyParser from "body-parser";
-import cors from "cors";
-import http from "http";
-import "dotenv-defaults/config.js";
-import path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { GraphQLServer, PubSub } from 'graphql-yoga';
+import * as db from './db';
+import mongo from './mongo';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
 
-import db from "./backend/db.js";
-import Query from "./backend/resolvers/Query.js";
-import Mutation from "./backend/resolvers/Mutation.js";
-import Subscription from "./backend/resolvers/Subscription.js";
-import mongo from "./backend/mongo.js";
-import apiRoute from "./backend/route/api.js";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const port = process.env.PORT || 80;
-
-const typeDefs = importSchema("./backend/schema.graphql");
 const pubsub = new PubSub();
-const app = express();
 
-app.use(cors());
-app.use("/api", apiRoute);
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-const server = new ApolloServer({
-  typeDefs,
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
@@ -44,13 +20,8 @@ const server = new ApolloServer({
   },
 });
 
-server.applyMiddleware({ app });
-const httpServer = http.createServer(app);
-server.installSubscriptionHandlers(httpServer);
+mongo();
 
-mongo.connect();
-
-httpServer.listen(port, () => {
-  console.log(`🚀 Server Ready at ${port}! 🚀`);
-  console.log(`Graphql Port at ${port}${server.subscriptionsPath}`);
+server.start({ port: process.env.PORT | 5000 }, () => {
+  console.log(`The server is up on port ${process.env.PORT | 5000}!`);
 });
