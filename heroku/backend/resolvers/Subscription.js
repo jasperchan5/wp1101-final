d64e34687a5f3bde2da460a29b1358326d1bfbd2
jasperchan5 <jscnn51011@gmail.com>
2022-01-13 22:46:20 +0800

    Add deploy

--- heroku/backend/resolvers/Subscription.js
+++
@@ -1,9 +0,0 @@
-const Subscription = {
-  people: {
-    subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator("PEOPLE");
-    },
-  },
-};
-
-export default Subscription;
--- heroku/src/graphql/index.js
+++
@@ -1,3 +0,0 @@
-export { INSERT_PEOPLE_MUTATION } from './mutations';
-export { STATS_COUNT_QUERY } from './queries';
-export { PEOPLE_SUBSCRIPTION } from './subscriptions';
