44dd4debd63db171fed510876cd276795b5f2ff1
Forbes Chou <seer0970202206@gmail.com>
2022-01-15 14:37:32 +0800

    delete all

--- src/graphql/index.js
+++
@@ -1,3 +0,0 @@
-export * from './queries'
-export * from './mutation'
-export * from './subscriptions'
--- src/graphql/subscriptions.js
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const TIME_SUBSCRIPTION = gql`
-    subscription time($team: String!) {
-        time(team: $team)
-    }
-`
